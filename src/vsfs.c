1f18b5e83751c3cad4885303374e9a412ccaa5fb
Cole <onslauth@gmail.com>
2014-05-24 19:04:28 +0200

    Adding license to the files.

--- src/vsfs.c
+++ src/vsfs.c
@@ -5,6 +5,33 @@
  * The environment variable specified should point to a real fully qualified path. This path is then used as the 
  * root path for all requests received by this fuse filesystem.
  *
+ * Copyright (c) 2014, Cole Minnaar and ﬀrøøt limited
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. All advertising materials mentioning features or use of this software
+ *     must display the following acknowledgement:
+ *     This product includes software developed by the Cole Minnaar and ﬀrøøt limited.
+ *  4. Neither the name ﬀrøøt nor the names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY COLE MINNAAR ''AS IS'' AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL COLE MINNAAR BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
 */
 
 #include "params.h"
@@ -26,9 +53,9 @@
 // Report errors to logfile and give -errno to caller
 static int vs_error( char *str)
 {
-    int ret = -errno;
-    
-    return ret;
+	int ret = -errno;
+
+	return ret;
 }
 
 // This function will take a pid and examine its /proc/pid/environ file
